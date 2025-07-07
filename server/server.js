7a449d8f5b4bacf0b126631ef6d6e4e681ede76a
harshit kumar <harshit.page@gmail.com>
2025-07-08 00:40:26 +0530

    -a

--- server/server.js
+++ server/server.js
@@ -1,19 +1,45 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-// Middleware
-app.use(function(req, res, next) {
-  console.log("middleware");
-  next();
-});
+// app.set("view engine", "ejs")
 
-// Route
-app.get("/profile/:username", function(req, res) {
-  res.send(`hello world ${req.params.username}`);
-});
+// // Middleware
+// app.use(function(req, res, next) {
+//   console.log("middleware");
+//   next();
+// });
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// // Route
+// app.get("/", function(req, res) {
+//   res.render("index");
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+// app.use(function errorHandler(err,req,res,next){
+//   if (res.headersSent){
+//     return next(err)
+//   }
+//   res.status(500)
+//   res.render('error',{error:err}) // here the error page will render
+// })
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set("view engine", "ejs")
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+  res.render('index');
+})
+
+app.listen(port,()=> {
+  console.log(`server is running at http://localhost:${port}`);
+})
