94317f2d83fc4ace1b4b3d99fc7ac8983c535956
harshit kumar <harshit.page@gmail.com>
2025-07-07 15:38:54 +0530

    -a

--- src/views/Tryon.jsx
+++ src/views/Tryon.jsx
@@ -11,8 +11,8 @@ function Tryon() {
         </video>
       </div>
       <div className='flex flex-col flex-1 justify-center md:items-start items-center text-left  h-full md:pl-20 relative bottom-[12vh] md:bottom-0'>
-        <div className='md:text-left text-center md:text-[2.8rem] text-[1.4rem] md:w-100 w-50 md:leading-14 leading-8 text-black'>swipe to <span className='italic text-[#FF444F]'>personalize</span></div>
-        <div className='md:text-[1.3rem] md:w-100 w-60 pt-4 text-gray-400 text-center md:text-left'>swipe right to save favorites, left to skip, and up to add to cart, personalized fashion made simple and fun!</div>
+        <div className='md:text-left text-center md:text-[2.8rem] text-[1.4rem] md:w-70 w-50 md:leading-14 leading-8 text-black'><span className='italic text-[#FF444F]'>try</span> before you buy</div>
+        <div className='md:text-[1.3rem] md:w-100 w-60 pt-4 text-gray-400 text-center md:text-left'>not sure if that outfit works on you? skip the guesswork , try it on virtually</div>
       </div>
     </div>
   );
